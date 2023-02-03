43c49b309b4601d0e0e6c8b0962e55b3629846cc
Ongraph ROR <rordev@ongraph.com>
2023-02-03 19:12:19 +0530

    first commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# app/controllers/static_pages_controller.rb
+class StaticPagesController < ApplicationController
+  skip_before_action :authenticate_user!
+  # GET '/landing_page'
+  def landing_page; end
+
+  # GET '/dashboard'
+  def dashboard; end
+end
